a7578e094d316159ed90dd28c112e00f8e72032b
Karen Jo <karenfiksdahl@gmail.com>
2023-01-15 15:53:31 +0100

    adding features to functions

--- js/object.js
+++ js/object.js
@@ -5,14 +5,36 @@ const backpack = {
   valuta: "NOK",
   onSale: true,
   isWaterProof: true,
-  features: ["Waterproof", "Flexible", "Breathable", "Reflective"],
-  ignition: function () {
-    backpack.on = !backpack.on;
-  },
-};
+  features: ["Waterproof", "Flexible", "Breathable", "Reflective"]
+  };
+
 
 // String: brand
 // Number: liters
 // Boolean: true
 // Array: features
 // Function:
+
+// * Function to add new features * //
+
+let features = ["Waterproof", "Flexible", "Breathable", "Reflective"];
+
+console.log (features);
+
+// add Windproof to feature by using array.push()
+
+features.push("Windproof");
+
+console.log(features);
+
+// * Function to access every item in an array by using a for loop
+
+
+for (let i = 0; i < features.length; i++) {
+    let fea = features[i];
+    console.log(fea);
+}
+
+// including added feature "Windproof"
+
+
